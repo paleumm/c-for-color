72a5060f83aa3e2c659f9c1ccc214edc4fb0fb32
paleumm <zdotqdot@gmail.com>
2022-05-29 16:07:47 +0700

    Update color.c

--- color.c
+++ color.c
@@ -5,7 +5,7 @@ int main(void)
     int color;
     for (color = 17; color < 232; color++)
     {
-        printf("\t\033[48;5;%dm COMPUTER!!\t\033[0m\n", color);
+        printf("\t\033[48;5;%dm PROGRAMMING!!\t\033[0m\n", color);
     }
     return 0;
 }
